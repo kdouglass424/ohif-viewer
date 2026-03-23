e0c05002474e79199be27d0f5b000815674ba393
Keith Douglass <kdouglass@vcradiology.com>
2026-03-23 15:44:34 -0400

    remove devcontainer

--- .devcontainer/postgres/init-pacs-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Create the 'pacs' database if it doesn't already exist.
-# This runs automatically on first postgres volume initialization.
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    SELECT 'CREATE DATABASE pacs'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'pacs')\gexec
-EOSQL
